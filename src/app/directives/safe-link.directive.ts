be8e224e2b381259a6f6452c400275c78c5317f9
Chema Muñoz <chemunoz78@gmail.com>
2024-07-18 19:04:43 +0200

    refactor: Access to the host element where Directive is apply and use it internally

--- src/app/directives/safe-link.directive.ts
+++ src/app/directives/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, input } from '@angular/core';
+import { Directive, ElementRef, inject, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -11,16 +11,17 @@ export class SafeLinkDirective {
   // We expose the input as 'appSafeLink' to the template but use 'myapp' internally
   queryParam = input('myapp', { alias: 'appSafeLink' });
 
+  // We access to the host element using ElementRef and modify its href attribute
+  #hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
   constructor() {}
 
   onConfirmLeavePage(event: MouseEvent): void {
     const wantsToLeave = confirm('Do you want to leave the app?');
 
     if (wantsToLeave) {
-      const address = (event.target as HTMLAnchorElement).href;
-      (
-        event.target as HTMLAnchorElement
-      ).href = `${address}?from=${this.queryParam()}`;
+      const address = this.#hostElementRef.nativeElement.href;
+      this.#hostElementRef.nativeElement.href = `${address}?from=${this.queryParam()}`;
       return;
     }
     event.preventDefault();
