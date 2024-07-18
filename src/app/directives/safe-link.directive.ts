6aa533c1cab70c3fe771f15c7fca7e91325d33b9
Chema Muñoz <chemunoz78@gmail.com>
2024-07-18 18:11:09 +0200

    feat: Add prevent unwanted navigation with a new Directive

---
+++ src/app/directives/safe-link.directive.ts
@@ -0,0 +1,19 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: 'a[appSafeLink]',
+  standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
+})
+export class SafeLinkDirective {
+  constructor() {}
+
+  onConfirmLeavePage(event: MouseEvent): void {
+    const wantsToLeave = confirm('Do you want to leave the app?');
+    if (!wantsToLeave) {
+      event.preventDefault();
+    }
+  }
+}
